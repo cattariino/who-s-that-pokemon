e7d1750b271866b86c64affee5196bdcbb35debd
grabii <camaleonipop@gmail.com>
2024-05-01 15:10:00 -0600

    cambios en el desing mobile

--- main.js
+++ main.js
@@ -130,6 +130,16 @@ function loadNewPokemon() {
 
                 button.addEventListener("click", () => {
                     if (name === correctName) {
+                        function randomInRange(min, max) {
+                            return Math.random() * (max - min) + min;
+                          }
+                          
+                          confetti({
+                            angle: randomInRange(55, 125),
+                            spread: randomInRange(50, 70),
+                            particleCount: randomInRange(50, 100),
+                            origin: { y: 0.6 }
+                          });
                         imgPokemon.style.filter = "none";
                         signoInterrogacion.textContent = correctName;
                         const allButtons = containerBotones.querySelectorAll("button");
